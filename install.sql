050e57ff9ef6461e720cae513aea744b80d048ad
Alex Redding <Alexredding180@gmail.com>
2021-07-24 19:23:16 +0100

    Changes I made probably ages ago

--- install.sql
+++ install.sql
@@ -27,16 +27,20 @@ INSERT INTO tc_site_map (page_id, module_id, parent_page_id, parent_page_module_
                          controller, action, display_name, page_small_icon, panelbar_icon, show_in_sidebar, view_order,
                          required_permissions, menu_required_permissions, page_manager, page_search_provider,
                          cache_name)
+VALUES (2, 'ad3d6004-3dce-414c-a246-bccd461ba853', 23, 'd3b2aa93-7e2b-4e0d-8080-67d14b2fa8a9', null,
+        '/Service/BackupManager/$[SelectedService.ServiceId]', '/Service/BackupManager/$[SelectedService.ServiceId]',
+        'BackupManager', 'Index', 'Backup Manager', 'MenuIcons/Base/ServerComponents24x24.png',
+        'MenuIcons/Base/ServerComponents16x16.png', 0, 100, null, null, null, null, null);
+INSERT INTO tc_site_map (page_id, module_id, parent_page_id, parent_page_module_id, category_id, url, mvc_url,
+                         controller, action, display_name, page_small_icon, panelbar_icon, show_in_sidebar, view_order,
+                         required_permissions, menu_required_permissions, page_manager, page_search_provider,
+                         cache_name)
 VALUES (1, 'ad3d6004-3dce-414c-a246-bccd461ba853', 40, '07405876-e8c2-4b24-a774-4ef57f596384', 1, '/BackupManagerAdmin',
         '/BackupManagerAdmin', 'BackupManagerAdmin', 'Index', 'Configure Backup Providers',
         'MenuIcons/Base/ServerComponents24x24.png', 'MenuIcons/Base/ServerComponents16x16.png', 1, 100,
         '({07405876-e8c2-4b24-a774-4ef57f596384,0,8})', '({07405876-e8c2-4b24-a774-4ef57f596384,0,8})', null, null,
         null);
-INSERT INTO tc_site_map (page_id, module_id, parent_page_id, parent_page_module_id, category_id, url, mvc_url,
-                         controller, action, display_name, page_small_icon, panelbar_icon, show_in_sidebar, view_order,
-                         required_permissions, menu_required_permissions, page_manager, page_search_provider,
-                         cache_name)
-VALUES (2, 'ad3d6004-3dce-414c-a246-bccd461ba853', 23, 'd3b2aa93-7e2b-4e0d-8080-67d14b2fa8a9', null,
-        '/Service/BackupManager/$[SelectedService.ServiceId]', '/Service/BackupManager/$[SelectedService.ServiceId]',
-        'BackupManager', 'Index', 'Backup Manager', 'MenuIcons/Base/ServerComponents24x24.png',
-        'MenuIcons/Base/ServerComponents16x16.png', 0, 100, null, null, null, null, null);
+
+-- ---------------------------------------------------------------------------------------------------------------------
+
+INSERT INTO tc_panelbar_categories (category_id, module_id, display_name, view_order, parent_category_id, parent_module_id, page_id, panelbar_icon) VALUES (1, 'ad3d6004-3dce-414c-a246-bccd461ba853', 'Backup Manager', 151, 6, '07405876-e8c2-4b24-a774-4ef57f596384', null, null);
